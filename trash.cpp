814d8083f8692f7adf6d0517cda130083662ded9
Christian Byrne <christianbyrne2012@gmail.com>
2015-05-13 09:16:47 -0400

    add prefixes/suffixes for synopsis

--- trash.cpp
+++ trash.cpp
@@ -18,6 +18,7 @@ str_to_int ( std::string str )
 int
 main ( int argc, char** argv )
 {
+	int		emphasis = 0;
 	std::string	input;
 
 	std::cout << "is it garbage, or is it trash?\n";
@@ -25,23 +26,44 @@ main ( int argc, char** argv )
 		std::cout << "?>";
 		std::cin >> input;
 		std::cout << '\n' << "synopsis: ";
+		emphasis	= str_to_int(input) % 128;
 
 		switch ( str_to_int(input) % 4 ) {
 			case 0:
+			if ( emphasis <= 32 )
+				std::cout << "sort of ";
+			else if ( emphasis <= 32*2 )
+				std::cout << "forgivable ";
+			else if ( emphasis <= 32*3 )
+				std::cout << "undeniable ";
+			else if ( emphasis <= 32*4 )
+				std::cout << "unforgivable ";
 			std::cout << "trash\n";
 			break;
 			case 1:
+			if ( emphasis <= 32 )
+				std::cout << "kind of ";
+			else if ( emphasis <= 32*2 )
+				std::cout << "probably ";
+			else if ( emphasis <= 32*3 )
+				std::cout << "definitely ";
+			else if ( emphasis <= 32*4 )
+				std::cout << "flaming ";
 			std::cout << "garbage\n";
 			break;
 			case 2:
-			std::cout << "vape\n";
+			std::cout << "vape ";
+			if ( emphasis <= 32 )
+				std::cout << "pen\n";
+			else if ( emphasis <= 32*4 )
+				std::cout << "smoke\n";
 			break;
 			case 3:
 			std::cout << "rubbish\n";
 			break;
 		}
 
-		std::cout << "emphasis: " << str_to_int(input) % 128 << " out of 128\n";
+		std::cout << "emphasis: " << emphasis << " out of 128\n";
 	}
 
 	return	0;
